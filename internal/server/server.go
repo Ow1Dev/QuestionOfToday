03fb3a5d64134273a3cf77f2634aa83e9d7e2f20
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-01-29 22:53:03 +0100

    fix: insert of handler we just use go build in route system

--- internal/server/server.go
+++ internal/server/server.go
@@ -26,19 +26,7 @@ func addRoutes(
 	mux *http.ServeMux,
 	repo *repository.Queries,
 ) {
-  mux.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
-	mux.Handle("/anwser", methodRestrict(http.MethodPost, handlers.HandleAnwser()))
-	mux.Handle("/", methodRestrict(http.MethodGet, handlers.HandleIndex(repo)))
+  mux.Handle("GET /static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./public"))))
+  mux.Handle("POST /anwser", handlers.HandleAnwser())
+  mux.Handle("GET /", handlers.HandleIndex(repo))
 }
-
-func methodRestrict(method string, next http.Handler) http.Handler {
-    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-        if r.Method != method {
-            http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
-            return
-        }
-        next.ServeHTTP(w, r)
-    })
-}
-
-
