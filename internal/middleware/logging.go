7d35516270e08ea8e638ac7669ecb44aa9fdeffa
Ow1Dev <31339803+Ow1Dev@users.noreply.github.com>
2025-02-24 17:12:55 +0000

    feat: adding so we are also logging the status

--- internal/middleware/logging.go
+++ internal/middleware/logging.go
@@ -7,16 +7,43 @@ import (
 	"github.com/rs/zerolog"
 )
 
+type loggingResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+	size       int
+}
+
+func (lrw *loggingResponseWriter) WriteHeader(code int) {
+	lrw.statusCode = code
+	lrw.ResponseWriter.WriteHeader(code)
+}
+
+func (lrw *loggingResponseWriter) Write(b []byte) (int, error) {
+	n, err := lrw.ResponseWriter.Write(b)
+	lrw.size += n
+	return n, err
+}
+
 func NewLoggingMiddleware(logger *zerolog.Logger, next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-    start := time.Now()
+		start := time.Now()
 
-		next.ServeHTTP(w, r)
+		lrw := &loggingResponseWriter{ResponseWriter: w}
+		next.ServeHTTP(lrw, r)
 
-    logger.Info().
-      Str("method", r.Method).
-      Str("url", r.URL.RequestURI()).
-      Dur("duration", time.Since(start)).
-      Msg("incoming request")
+		var logEvent *zerolog.Event
+		if lrw.statusCode >= 500 {
+			logEvent = logger.Error()
+		} else {
+			logEvent = logger.Info()
+		}
+
+		logEvent.
+			Str("method", r.Method).
+			Str("url", r.URL.RequestURI()).
+			Int("status", lrw.statusCode).
+			Int("size", lrw.size).
+			Dur("duration", time.Since(start)).
+			Msg("incoming request")
 	})
 }
